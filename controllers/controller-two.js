2dcb826bc8a8782953d091030b9a794e16934193
Gabriel Ortega Rosero <gabrielo_9103@hotmail.com>
2019-01-24 23:42:47 -0500

    approach with classes for importing modules

---
+++ controllers/controller-two.js
@@ -0,0 +1,17 @@
+'stric'
+
+
+
+class ControllerTwo {
+    getRouteTwoInfo(req, res, next) {
+        const { params } = req.params
+
+        if (params !== '5') { 
+          return next(new Error('Something not found'))
+        }
+      
+        res.send({ params })
+    }
+}
+
+module.exports = new ControllerTwo()
